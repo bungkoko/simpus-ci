479d91935d176068244f7053878cbaf0d66313cf
Bung Koko <joko.purwanto18@gmail.com>
2017-02-20 23:40:46 +0700

    modif css paginasi all member

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -186,7 +186,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         $config['per_page']='10';
         $config['num_links']='6';
 
+        $config['full_tag_open'] = '<div class="pagination"><ul>';
+      $config['full_tag_close'] = '</ul></div>';
 
+      $config['first_link'] = '&laquo; First';
+      $config['first_tag_open'] = '<li class="prev page">';
+      $config['first_tag_close'] = '</li>';
+
+      $config['last_link'] = 'Last &raquo;';
+      $config['last_tag_open'] = '<li class="next page">';
+      $config['last_tag_close'] = '</li>';
+
+      $config['next_link'] = 'Next &rarr;';
+      $config['next_tag_open'] = '<li class="next page">';
+      $config['next_tag_close'] = '</li>';
+
+      $config['prev_link'] = '&larr; Previous';
+      $config['prev_tag_open'] = '<li class="prev page">';
+      $config['prev_tag_close'] = '</li>';
+
+      $config['cur_tag_open'] = '<li class="active"><a href="">';
+      $config['cur_tag_close'] = '</a></li>';
+
+      $config['num_tag_open'] = '<li class="page">';
+      $config['num_tag_close'] = '</li>';
         //initialize pagination
         $this->pagination->initialize($config);
         $data['all']=$this->Member_md->get_all($config['per_page'],$this->uri->segment(3));
